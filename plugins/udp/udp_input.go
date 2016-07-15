e2096f39b82fef7a9f69aa55139ec1dd023b3d3e
Xavier Lange <xrlange@gmail.com>
2016-07-15 12:35:40 -0700

    why did udp complain?

--- plugins/udp/udp_input.go
+++ plugins/udp/udp_input.go
@@ -23,7 +23,7 @@ import (
 	"strconv"
 	"strings"
 
-	. "github.com/mozilla-services/heka/message"
+	// . "github.com/mozilla-services/heka/message"
 	. "github.com/mozilla-services/heka/pipeline"
 )
 
